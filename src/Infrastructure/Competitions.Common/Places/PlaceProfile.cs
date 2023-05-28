8dbf42de9b447c7cb95b9f8ab2740650ecf7b4de
Amirreza-Asp <m.senator.6247@gmail.com>
2023-05-28 09:44:15 +0330

    fix place image bug

--- src/Infrastructure/Competitions.Common/Places/PlaceProfile.cs
+++ src/Infrastructure/Competitions.Common/Places/PlaceProfile.cs
@@ -11,30 +11,30 @@ namespace Competitions.Common.Places
     public class PlaceProfile : Profile
     {
 
-        public PlaceProfile ()
+        public PlaceProfile()
         {
-            CreateMap<Place , GetPlaceDto>()
-                .ForMember(dto => dto.Image , entity => entity.MapFrom(u => u.Images.ElementAt(0).Image.Name))
-                .ForMember(dto => dto.SupervisorName , entity => entity.MapFrom(u => u.Supervisor.Name));
+            CreateMap<Place, GetPlaceDto>()
+                .ForMember(dto => dto.Image, entity => entity.MapFrom(u => u.Images.Any() ? u.Images.ElementAt(0).Image.Name : ""))
+                .ForMember(dto => dto.SupervisorName, entity => entity.MapFrom(u => u.Supervisor.Name));
 
             // Create Place
-            CreateMap<Place , CreatePlaceDto>();
-            CreateMap<Supervisor , CreateSupervisorDto>()
-                .ForMember(dto => dto.PhoneNumber , entity => entity.MapFrom(u => u.PhoneNumber.Value));
+            CreateMap<Place, CreatePlaceDto>();
+            CreateMap<Supervisor, CreateSupervisorDto>()
+                .ForMember(dto => dto.PhoneNumber, entity => entity.MapFrom(u => u.PhoneNumber.Value));
 
             // Update Place
-            CreateMap<Place , UpdatePlaceDto>()
-                .ForMember(dto => dto.CurrentImages , entity => entity.MapFrom(u => u.Images))
-                .ForMember(dto => dto.NewSports , entity => entity.MapFrom(u => String.Join(',' , u.Sports.Select(b => b.Sport.Id))));
-            CreateMap<PlaceImages , PlaceImageDto>()
-                .ForMember(dto => dto.Name , entity => entity.MapFrom(u => u.Image.Name));
-            CreateMap<PlaceSports , SelectListItem>()
-                .ForMember(dto => dto.Text , entity => entity.MapFrom(u => u.Sport.Name))
-                .ForMember(dto => dto.Value , entity => entity.MapFrom(u => u.SportId));
+            CreateMap<Place, UpdatePlaceDto>()
+                .ForMember(dto => dto.CurrentImages, entity => entity.MapFrom(u => u.Images))
+                .ForMember(dto => dto.NewSports, entity => entity.MapFrom(u => String.Join(',', u.Sports.Select(b => b.Sport.Id))));
+            CreateMap<PlaceImages, PlaceImageDto>()
+                .ForMember(dto => dto.Name, entity => entity.MapFrom(u => u.Image.Name));
+            CreateMap<PlaceSports, SelectListItem>()
+                .ForMember(dto => dto.Text, entity => entity.MapFrom(u => u.Sport.Name))
+                .ForMember(dto => dto.Value, entity => entity.MapFrom(u => u.SportId));
 
             // Activity Plan
-            CreateMap<ActivityPlan , ActivityPlanDto>()
-                .ForMember(dto => dto.File , entity => entity.MapFrom(u => u.File.Name));
+            CreateMap<ActivityPlan, ActivityPlanDto>()
+                .ForMember(dto => dto.File, entity => entity.MapFrom(u => u.File.Name));
         }
 
     }
